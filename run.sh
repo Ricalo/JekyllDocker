5005d8821a54f76544f2d5b6aadf4c00206a1326
Ricardo Loo <ricalox@gmail.com>
2017-01-08 09:27:23 -0800

    Added a few optimizations to build and test
    * Added **NOKOGIRI_USE_SYSTEM_LIBRARIES** flag to bundle install
    * Moved build + test to its own process separated from the serve process

--- run.sh
+++ run.sh
@@ -6,8 +6,8 @@ set -o nounset
 # set -o xtrace
 
 set -x
-bundle install
-jekyll build
-# Validate HTML, but continue execution even if htmlproofer reports errors
-htmlproofer ./_site || true &
+env NOKOGIRI_USE_SYSTEM_LIBRARIES=true bundle install
+# Validate HTML in a folder outside of the mapped volume
+# serve the site at the same time
+jekyll build --destination ../test && htmlproofer ./_site || true &
 jekyll "$@"
